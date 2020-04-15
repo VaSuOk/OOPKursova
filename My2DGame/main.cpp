9dd92928ccd98fe92c429740b6031d7217ad5cd1
VaSuOk <vasuok15vas@gmail.com>
2020-04-15 18:30:48 +0300

    ADD_Lib

--- My2DGame/main.cpp
+++ My2DGame/main.cpp
@@ -4,31 +4,18 @@
 using namespace std;
 
 const int WIDTH = 800, HEIGHT = 600;
+Game *game = nullptr;
 
 int main(int argc, char* argv[])
 {
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) cout<<"ne Pashe"<<SDL_GetError()<<endl;
-    SDL_Window *window = SDL_CreateWindow("MyGame",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH ,HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    game = new Game();
 
-    if(NULL == window){
-        cout<<"Not Created"<<SDL_GetError()<<endl;
-        return EXIT_FAILURE;
+    game->Init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, false);
+    while(game->Running()){
+        game->HandlEvents();
+        game->Update();
+        game->Render();
     }
-
-    SDL_Event windowEvent;
-    while(true){
-        if(SDL_PollEvent( &windowEvent)){
-            if(SDL_QUIT == windowEvent.type){
-                break;
-            }
-
-        }
-    }
-
-
-
-    SDL_DestroyWindow( window );
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
+    game->Clean();
+    return 0;
 }
